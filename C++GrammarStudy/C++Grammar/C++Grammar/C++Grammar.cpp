0c218dcaaa422fed895b08456782bf91224ec3df
Seoptank <cnpdi1118@naver.com>
2024-05-21 00:35:50 +0900

    aaaa

--- C++GrammarStudy/C++Grammar/C++Grammar/C++Grammar.cpp
+++ C++GrammarStudy/C++Grammar/C++Grammar/C++Grammar.cpp
@@ -1,23 +1,26 @@
 ﻿#include <iostream>
 
-int main(void)
+using namespace std;
+
+// [타입][이름]
+
+// char					:1byte
+// short				:2byte
+// int					:4byte
+// __int64(long long)	:8byte
+
+int	hp;
+int	maxHp;
+
+
+int main()
 {
+	maxHp = 100;
 
-	variable();
+	hp = maxHp;
 
+	cout << hp;
 
 	return 0;
 }
 
-void variable()
-{
-	// int: 자료형
-	//	i : 변수명
-	// 자료형 - data를 표현하는 방식(크기 단위: byte)
-	// 정수형: char(1), short(2), int(4), long(4), long long(8)
-	// 실수형: float(4), double(8)
-
-	// 1 byte = 8
-
-	int i = 0;
-}
